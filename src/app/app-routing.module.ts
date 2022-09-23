0eb83577196a9e3911bdb9cd05d1aac3fe3b9b33
Daipha00 <msagaladaines@gmail.com>
2022-09-23 02:29:30 -0700

    Application License

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes =[
+  {
+    path:'',
+    component:HomeComponent
+},
+{
+  path:'users',
+  component:UsersComponent
+},
+{
+  path:'about',
+  component:AboutComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
